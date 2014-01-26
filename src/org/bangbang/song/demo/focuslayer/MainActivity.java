3b3aeffa0cabec2a34de49a815f15d1453eacdf8
bysong <bangbangiandroid@gmail.com>
2014-01-26 18:45:11 +0800

    WIP

--- src/org/bangbang/song/demo/focuslayer/MainActivity.java
+++ src/org/bangbang/song/demo/focuslayer/MainActivity.java
@@ -1,9 +1,13 @@
 
 package org.bangbang.song.demo.focuslayer;
 
-import android.os.Bundle;
+import org.bangbang.song.focuslayer.Utils;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalFocusChangeListener;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +15,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        getWindow().getDecorView().getViewTreeObserver().addOnGlobalFocusChangeListener(new OnGlobalFocusChangeListener() {
+            
+            @Override
+            public void onGlobalFocusChanged(View oldFocus, View newFocus) {
+                Utils.onFocusChange(oldFocus, false);
+                Utils.onFocusChange(newFocus, true);
+            }
+        });
     }
 
     @Override
---
+++ src/org/bangbang/song/focuslayer/Utils.java
@@ -0,0 +1,19 @@
+package org.bangbang.song.focuslayer;
+
+import org.bangbang.song.demo.focuslayer.R;
+
+import android.view.View;
+
+
+public class Utils {
+    public static final int FOCUS_LAYER_ID = R.id.menu_settings;
+    
+    public static void onFocusChange(View v, boolean hasFocus) {
+        if (null == v) {
+            return;
+        }
+        
+        IFocusAnimationLayer layper = (IFocusAnimationLayer) v.getRootView().findViewById(FOCUS_LAYER_ID);
+        layper.onFocusChange(v, hasFocus);
+    }
+}
