b9525f8771c82326e9631a3929629f0b56d34f1e
bysong <bangbangiandroid@gmail.com>
2014-02-18 19:31:46 +0800

    add FocusViewGroup demo.

---
+++ src/org/bangbang/song/focuslayer/AnimationFocusViewGroup.java
@@ -0,0 +1,108 @@
+package org.bangbang.song.focuslayer;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.widget.AbsoluteLayout;
+import android.widget.AbsoluteLayout.LayoutParams;
+
+public class AnimationFocusViewGroup extends FocusViewGroup {
+    private static final String TAG = AnimationFocusViewGroup.class.getSimpleName();
+    public AnimationFocusViewGroup(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+    }
+
+    public AnimationFocusViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+    public AnimationFocusViewGroup(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    protected void doScalDown(View v) {
+        super.doScalDown(v);
+        
+        Rect r = (Rect) v.getTag(ID_RECT);
+        if (null == r) {
+            return;
+        }
+        
+        updateParam(v, r);
+        
+        Rect rect = r;
+        Rect scaledRect = mConfigure.mLastScaledFocusRect;
+        
+        float fromX = (float) scaledRect.width() / rect.width();
+        float toX = 1;
+        float fromY = (float) scaledRect.height() / rect.height();
+        float toY = 1;
+        float pivotX = .5f;
+        float pivotY = .5f;
+        if (mConfigure.DEBUG_SCALE_ANIMATION) {
+            Log.d(TAG, "scale fromX: " + fromX + " toX: " + toX + " fromY: " + fromY + " toY: "
+                    + toY + " pivotX: " + pivotX + " pivotY: " + pivotY);
+        }
+        ScaleAnimation s = new ScaleAnimation(fromX, toX, fromY, toY, Animation.RELATIVE_TO_SELF,
+                pivotX, Animation.RELATIVE_TO_SELF, pivotY);
+        s.setDuration(mConfigure.mDuration);
+        s.setInterpolator(new LinearInterpolator());
+        v.startAnimation(s);
+    }
+    
+    @Override
+    protected void doScalUp(View v) {
+        super.doScalUp(v);
+        
+        Rect rect = mConfigure.mCurrentScaledFocusRect;
+        updateParam(v, rect);
+        
+        rect = (Rect) v.getTag(ID_RECT);
+        Rect scaledRect = mConfigure.mCurrentScaledFocusRect;
+        
+        float fromX = (float) rect.width() / scaledRect.width();
+        float toX = 1;
+        float fromY = (float) rect.height() / scaledRect.height();
+        float toY = 1;
+        float pivotX = scaledRect.exactCenterX();
+        float pivotY = scaledRect.exactCenterY();
+        
+        pivotX = 0.5f;
+        pivotY = 0.5f;
+        if (mConfigure.DEBUG_SCALE_ANIMATION) {
+            Log.d(TAG, "scale fromX: " + fromX + " toX: " + toX + " fromY: " + fromY + " toY: "
+                    + toY + " pivotX: " + pivotX + " pivotY: " + pivotY);
+        }
+        ScaleAnimation s = new ScaleAnimation(fromX, toX, fromY, toY, Animation.RELATIVE_TO_SELF,
+                pivotX, Animation.RELATIVE_TO_SELF, pivotY);
+        s.setDuration(mConfigure.mDuration);
+        s.setInterpolator(new LinearInterpolator());
+        v.startAnimation(s);
+    }
+
+    private void updateParam(View v, Rect r) {
+        LayoutParams params = null;
+
+        int width = r.width();
+        int height = r.height();
+        int x = r.left;
+        int y = r.top;
+        if (mConfigure.DEBUG_TRANSFER_ANIMATION) {
+            Log.d(TAG, "new layout params x: " + x + " y: " + y + " width: " + width + " height: "
+                    + height);
+        }
+        params = new AbsoluteLayout.LayoutParams(width, height, x, y);
+        updateViewLayout(v, params);
+    }
+    
+    
+}
---
+++ src/org/bangbang/song/focuslayer/FocusViewGroup.java
@@ -0,0 +1,81 @@
+package org.bangbang.song.focuslayer;
+
+import org.bangbang.song.demo.focuslayer.R;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AbsoluteLayout;
+
+public class FocusViewGroup extends AbsoluteLayout implements IFocusAnimationLayer {
+    private static final String TAG = FocusViewGroup.class.getSimpleName();
+    
+    public static final int ID_RECT = R.id.paste;
+    protected AnimationConfigure mConfigure;
+
+    private OnFocusChangeListener mListener;
+    public FocusViewGroup(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        
+        init();
+    }
+
+    public FocusViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        
+        init();
+    }
+
+    public FocusViewGroup(Context context) {
+        super(context);
+        
+        init();
+    }
+    
+    void init() {
+        mConfigure = new AnimationConfigure();
+        mConfigure.mScaleFactor = 3.0f;
+        mListener = new OnFocusChangeListener() {
+            
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                FocusViewGroup.this.onFocusChange(v, hasFocus);
+            }
+        };
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (v.getParent() != this) {
+            throw new IllegalStateException("view must be my child.");
+        }
+        
+        if (hasFocus) {
+            mConfigure.onNewFocus(this, v);
+            if (v.getTag(ID_RECT)  == null) {
+                v.setTag(ID_RECT, new Rect(v.getLeft(), v.getTop(),
+                        v.getWidth() + v.getLeft(), v.getTop() + v.getHeight()));
+            }
+            doScalUp(v);
+        } else {
+            doScalDown(v);
+        }
+    }
+
+    protected void doScalDown(View v) {
+        
+    }
+
+    protected void doScalUp(View v) {
+        v.bringToFront();
+    }
+    
+    @Override
+    public void addView(View child, int index, android.view.ViewGroup.LayoutParams params) {
+        // TODO Auto-generated method stub
+        super.addView(child, index, params);
+        
+        child.setOnFocusChangeListener(mListener);
+    }
+}
